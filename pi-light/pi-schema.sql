cba89f8f6ea4f7ce525a8d593c487c46e01e1e41
TradingToolCrypto <67847059+TradingToolCrypto@users.noreply.github.com>
2021-06-14 17:13:02 +0700

    Delete pi-light directory

--- pi-light/pi-schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;
-
-CREATE TABLE pi_obs(
-  time timestamptz,
-  metric text,
-  value numeric);
-  
-SELECT create_hypertable('pi_obs', 'time', chunk_time_interval=>'1 week');
