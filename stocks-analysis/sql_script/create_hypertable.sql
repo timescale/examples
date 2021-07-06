dfb66fbf5ead1b0a659a8fd1513b9827f8a5e698
Attila Toth <hello@attilatoth.dev>
2021-07-06 19:09:33 +0200

    update table name and column

--- stocks-analysis/sql_script/create_hypertable.sql
+++ stocks-analysis/sql_script/create_hypertable.sql
@@ -5,4 +5,4 @@ CREATE EXTENSION IF NOT EXISTS timescaledb;
 Turn the 'stocks2' table into a hypertable.
 This is important to be able to make use of TimescaleDB features later on.
 */
-SELECT create_hypertable('stocks2', 'stock_datetime');
+SELECT create_hypertable('stocks_intraday', 'time');
