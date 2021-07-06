a63f6242c58ba4f65c62631fb0715d42ff00f44f
Attila Toth <hello@attilatoth.dev>
2021-07-06 19:30:38 +0200

    Typo

--- stocks-analysis/config.py
+++ stocks-analysis/config.py
@@ -1,4 +1,4 @@
-# Make sure to edit this configureation file with your database connection details
+# Make sure to edit this configuration file with your database connection details
 # and Alpha Vantage API key
 DB_USER = 'user'
 DB_PASS = 'passwd'
