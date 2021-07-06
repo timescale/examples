7619806cb4610aca82cfa769d1ceb48672f41ac4
Attila Toth <hello@attilatoth.dev>
2021-07-06 18:55:54 +0200

    Update config file

--- stocks-analysis/config.py
+++ stocks-analysis/config.py
@@ -1,8 +1,8 @@
-
-class StocksConfig(object):
-    DB_USER = 'user'
-    DB_PASS = 'passwd'
-    DB_HOST = 'host'
-    DB_PORT = 'port'
-    DB_NAME = 'name'
-    APIKEY = 'apikey'
+# Make sure to edit this configureation file with your database connection details
+# and Alpha Vantage API key
+DB_USER = 'user'
+DB_PASS = 'passwd'
+DB_HOST = 'host'
+DB_PORT = '000'
+DB_NAME = 'db'
+APIKEY = 'alpha_vantage_apikey'
