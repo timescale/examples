8ed6e64a27f807830dca87329315cf2cd2b34b27
Attila Toth <hello@attilatoth.dev>
2021-07-16 14:44:24 +0200

    config

--- analyze-intraday-stocks/dashboard/config.py
+++ analyze-intraday-stocks/dashboard/config.py
@@ -1,8 +1,8 @@
 # Make sure to edit this configuration file with your database connection details
 # and Alpha Vantage API key
-DB_USER = 'tsdbadmin'
-DB_PASS = 'bh41gyz4vtle98ly'
-DB_HOST = 'kgvwbcip2d.y2c9uxtgd2.tsdb.forge.timescale.com'
-DB_PORT = '36999'
-DB_NAME = 'tsdb'
-APIKEY = '17M94CR8BM73GSDB'  # or this 'EVPYRNBZ4RXWDZGW'
+DB_USER = ''
+DB_PASS = ''
+DB_HOST = ''
+DB_PORT = ''
+DB_NAME = ''
+APIKEY = ''  
